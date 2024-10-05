4992786c97480dd6e4bdf6eca7f3ad03dedece0a
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-10-05 13:31:48 -0300

    Feat: adicionar enum para status das solicitações de amizade
    - Criado enum FriendRequestStatus para gerenciar os estados: Pending, Accepted, Rejected, e Blocked

---
+++ WebApiMessage-Chat/Enums/FriendRequestStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiMessage_Chat.Enums;
+
+public enum FriendRequestStatus
+{
+    Pending = 1,
+    Accepted = 2,
+    Rejected = 3,
+    Blocked = 4
+}
--- WebApiMessage-Chat/Models/Friendship.cs
+++ WebApiMessage-Chat/Models/Friendship.cs
@@ -7,7 +7,7 @@ public class Friendship
     public int Id { get; set; }
     public int RequestId { get; set; }
     public int ReceiverId { get; set; }
-    public RequestedStatus Status { get; set; }
+    public FriendRequestStatus Status { get; set; }
     public DateTime RequestedAt { get; set; }
     public DateTime RespondedAt { get; set; }
 }
